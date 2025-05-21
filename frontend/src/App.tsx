f456459eb3298540c34b2958e420deaeba3bad43
fatima <assouabfatima19@gmail.com>
2025-05-21 10:39:02 +0100

    feat:creat sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { LoginForm } from "./components/login-form"
-import { Register } from "./pages/Register"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { LoginForm } from "./components/login-form";
+import { Register } from "./pages/Register";
+import Layout from "@/components/Layout";
 function App() {
 
   return (
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginForm/>} />
         <Route path="/register" element={<Register/>} />
-       
+         <Route path="/dashboard" element={<Layout children={undefined}/>} />
       </Routes>
     </BrowserRouter>
     </>
---
+++ frontend/src/components/app-sidebar.tsx
@@ -0,0 +1,69 @@
+ import { Book, Home,Calendar, Search, Settings, User } from "lucide-react"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Courses",
+    url: "#",
+    icon: Book,
+  },
+  {
+    title: "Event",
+    url: "#",
+    icon: Calendar,
+  },
+  
+  {
+    title: "Users",
+    url: "#",
+    icon: User,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon:Search ,
+  }, {
+    title: " Settings",
+    url: "#",
+    icon: Settings,
+  },
+]
+ 
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>JADARA</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
